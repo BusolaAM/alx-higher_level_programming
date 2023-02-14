b58df145ebd1d4af2c7a9b1a914c339ad96538a8
BusolaAM <busolaatinsola@gmail.com>
2023-02-14 21:14:57 +0100

    Cheating is bad

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
 -- Updates the score of Bob to 10 in the table second_table in my MySQL server.
-UPDATE `SECOND_TABLE`
-SET `SCORE` = 10
-WHERE `NAME` = "Bob";
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
