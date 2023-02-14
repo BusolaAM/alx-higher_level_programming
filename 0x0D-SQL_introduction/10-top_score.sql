64e51be5812e6cbc32c347f10a2162969e923d81
BusolaAM <busolaatinsola@gmail.com>
2023-02-14 21:11:35 +0100

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table second_table.
 -- Records are ordered by descending score.
-SELECT `SCORE`, `NAME`
-FROM `SECOND_TABLE`
-ORDER BY `SCORE` DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
