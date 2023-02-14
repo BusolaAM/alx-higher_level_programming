d3552b4946352a7cff71eb224cd0d081dfb21070
BusolaAM <busolaatinsola@gmail.com>
2023-02-14 21:00:10 +0100

    First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Inserts a new row into the table first_table in my MySQL server.
-INSERT INTO `FIRST_TABLE` (`ID`, `NAME`) VALUES (89, "Best School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
