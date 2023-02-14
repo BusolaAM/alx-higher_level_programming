bf3bb69e5fe4ec76f4f6f9a425617a629d592a63
BusolaAM <busolaatinsola@gmail.com>
2023-02-14 20:58:55 +0100

    Count 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Displays the number of records with id = 89 in the table first_table in my MySQL server.
 SELECT COUNT(*)
-FROM `FIRST_TABLE`
-WHERE `ID` = 89;
+FROM `first_table`
+WHERE `id` = 89;
