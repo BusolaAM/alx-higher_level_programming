81f2fd2c5cb497496048006049c96322cb728a4a
BusolaAM <busolaatinsola@gmail.com>
2023-03-13 19:47:50 +0100

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const argc = process.argv.length;
 
 if (argc > 2) {
-   console.log(`Arguments found: ${argc - 2}`);
+   console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-   console.log('No arguments found');
+   console.log('No argument');
 }
