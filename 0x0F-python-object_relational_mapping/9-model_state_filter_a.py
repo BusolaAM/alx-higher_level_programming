af4bff2325cea5881a75d4d04143b3ba8f4e98d4
BusolaAM <busolaatinsola@gmail.com>
2023-03-19 23:30:52 +0100

    9. Contains `a`

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-This script lists all State objects that contain the letter `a` from the database `hbtn_0e_6_usa`.
+This script lists all State objects
+that contain the letter `a`
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
